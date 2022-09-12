7be497caa73e1237104ec5a06ca289b65cf9b627
Kadidiatou SAMAKE <ksamake18@gmail.com>
2022-09-12 12:53:48 +0000

    commit fin tirage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { TirageComponent } from './tirage/tirage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tirage', component: TirageComponent},
+  {path: 'accueil', component: AccueilComponent},
+  {path: '', component: AccueilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,15 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { TirageComponent } from './tirage/tirage.component';
-=======
 import { AccueilComponent } from './accueil/accueil.component'
->>>>>>> 464f18a00eb8f8e0b245a37bc406017881fac4b3
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    TirageComponent
-=======
+    TirageComponent,
     AccueilComponent
     
->>>>>>> 464f18a00eb8f8e0b245a37bc406017881fac4b3
   ],
   imports: [
     BrowserModule,
