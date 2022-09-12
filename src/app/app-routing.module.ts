9891d4e3c6343bb3d7fe5f3d6e372537e5923287
Kadidiatou SAMAKE <ksamake18@gmail.com>
2022-09-12 15:55:30 +0000

    detail liste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
+import { DetailTirageComponent } from './detail-tirage/detail-tirage.component';
 import { TirageComponent } from './tirage/tirage.component';
 
 const routes: Routes = [
   {path: 'tirage', component: TirageComponent},
   {path: 'accueil', component: AccueilComponent},
+  {path: 'detail-tirage', component: DetailTirageComponent},
   {path: '', component: AccueilComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TirageComponent } from './tirage/tirage.component';
-import { AccueilComponent } from './accueil/accueil.component'
+import { AccueilComponent } from './accueil/accueil.component';
+import { DetailTirageComponent } from './detail-tirage/detail-tirage.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TirageComponent,
-    AccueilComponent
+    AccueilComponent,
+    DetailTirageComponent
     
   ],
   imports: [
